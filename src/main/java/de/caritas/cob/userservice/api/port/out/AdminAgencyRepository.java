c7da8aa49056ed23afe08e39120771e9274cca1f
Frank Gerhardt <Storypapst@users.noreply.github.com>
2026-07-08 10:30:16 +0200

    Fix agency admin relation projection query

--- src/main/java/de/caritas/cob/userservice/api/port/out/AdminAgencyRepository.java
+++ src/main/java/de/caritas/cob/userservice/api/port/out/AdminAgencyRepository.java
@@ -4,6 +4,7 @@ import de.caritas.cob.userservice.api.model.AdminAgency;
 import de.caritas.cob.userservice.api.model.AdminAgency.AdminAgencyBase;
 import java.util.List;
 import java.util.Set;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,6 +20,9 @@ public interface AdminAgencyRepository extends CrudRepository<AdminAgency, Long>
   @Transactional
   void deleteByAdminId(String adminId);
 
-  @SuppressWarnings("all")
+  @Query(
+      "SELECT adminAgency.id AS id, adminAgency.agencyId AS agencyId, adminAgency.admin.id AS adminId "
+          + "FROM AdminAgency adminAgency "
+          + "WHERE adminAgency.admin.id IN :adminIds")
   List<AdminAgencyBase> findByAdminIdIn(Set<String> adminIds);
 }
