eb111278ff3b87c6c29d37b701bdf5973a665429
Frank Gerhardt <Storypapst@users.noreply.github.com>
2026-07-26 04:23:11 +0200

    refactor: start group chats through Matrix

---
+++ src/test/java/de/caritas/cob/userservice/api/MatrixOnlyGroupChatLifecycleContractTest.java
@@ -0,0 +1,25 @@
+package de.caritas.cob.userservice.api;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.junit.jupiter.api.Test;
+
+class MatrixOnlyGroupChatLifecycleContractTest {
+
+  @Test
+  void startingAGroupChatMustNotCallRocketChat() throws IOException {
+    var source =
+        Files.readString(
+            Path.of("src/main/java/de/caritas/cob/userservice/api/facade/StartChatFacade.java"));
+
+    assertThat(source)
+        .doesNotContain(
+            "RocketChatService",
+            "RocketChatAddUserToGroupException",
+            "rocketChatService",
+            "Rocket.Chat");
+  }
+}
