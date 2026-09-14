abc05c5582e8774d2b42e70938a4c22e34bb857b
Shazia Kausar <dt1@dreambau.com>
2026-09-14 11:22:09 +0200

    fix: keep tenant contract provider-compatible

--- src/test/java/de/caritas/cob/userservice/api/service/TenantAdminCaseHandoverPolicyContractTest.java
+++ src/test/java/de/caritas/cob/userservice/api/service/TenantAdminCaseHandoverPolicyContractTest.java
@@ -32,7 +32,6 @@ class TenantAdminCaseHandoverPolicyContractTest {
     assertThat(reasonRequired).doesNotContain("clientConsent");
     var legacyConsent = (Map<String, Object>) reasonProperties.get("clientConsentRequired");
     assertThat(legacyConsent).containsEntry("deprecated", true);
-    assertThat((List<Map<String, Object>>) legacyConsent.get("allOf"))
-        .containsExactly(Map.of("$ref", "#/components/schemas/BooleanPermissionPolicy"));
+    assertThat(legacyConsent).containsEntry("$ref", "#/components/schemas/BooleanPermissionPolicy");
   }
 }
